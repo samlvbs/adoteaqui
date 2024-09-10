a15db1ef87340f8b11fe3bd66c6eae324ca9738e
Samuel Vilas Boas Souza <113648081+samlvbs@users.noreply.github.com>
2024-09-09 22:16:34 -0300

    Added form on home page to search for pets

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../shared/nav/nav.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, FooterComponent],
+  imports: [NavComponent, FooterComponent, ReactiveFormsModule, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  findForm = new FormGroup({
+    tipo: new FormControl(''),
+    local: new FormControl('')
+  })
 
+  handleSubmit(){
+    alert('Tipo do pet: '+ this.findForm.value.tipo + ' Localidade: ' + this.findForm.value.local)
+  }
 }
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
