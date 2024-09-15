d3083dbb31209c26bd15db102dc9d23720e8ff84
Samuel Vilas Boas Souza <113648081+samlvbs@users.noreply.github.com>
2024-09-15 18:13:22 -0300

    Implemented public API consumption for state and city. Running search form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { NavComponent } from '../../shared/nav/nav.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
-import { ToggleComponent } from '../../shared/components/toggle/toggle.component';
 import { StatesService } from '../../services/states.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSelect, MatOption } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { CitiesService } from '../../services/cities.service';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, FooterComponent, ToggleComponent, MatSelect, MatOption, ReactiveFormsModule, ButtonComponent, HttpClientModule, CommonModule],
+  imports: [NavComponent, FooterComponent, MatSelect, MatOption, MatSlideToggle, ReactiveFormsModule, ButtonComponent, HttpClientModule, CommonModule, MatFormFieldModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   providers: [StatesService]
@@ -22,29 +23,43 @@ import { CitiesService } from '../../services/cities.service';
 export class HomeComponent implements OnInit {
   states: any[] = [];
   cities: any[] = [];
+  selectedState: string =''
+
+  private _formBuilder = inject(FormBuilder);
+
   constructor(
     private _statesService: StatesService,
     private _citiesService: CitiesService
   ){}
 
   ngOnInit(){
-      this._statesService.getStates().subscribe((statesResponse) =>{
-        this.states = statesResponse;
-        console.log('Estados:  ', statesResponse);
-      })
-
-      this._citiesService.getCities('São paulo').subscribe((citiesResponse) =>{
-        this.cities = citiesResponse;
-        console.log('Cidades: ', citiesResponse)
-      })
+    this._statesService.getStates().subscribe((statesResponse) =>{
+      this.states = statesResponse;
+      console.log('Estados:  ', statesResponse);
+    })
   }
 
+  onStateSelected(state: string){
+    this.selectedState = state;
+
+    this._citiesService.getCities(state).subscribe((citiesResponse) =>{
+      this.cities = citiesResponse;
+      console.log('Cidades: ', citiesResponse)
+    })
+  }
   findForm = new FormGroup({
-    tipo: new FormControl(''),
-    local: new FormControl('')
+    citieSelect: new FormControl(''),
+    stateSelect: new FormControl(''),
+    enableDog: new FormControl(false),
+    enableCat: new FormControl(false)
   })
 
+
   handleSubmit(){
-    alert('Tipo do pet: '+ this.findForm.value.tipo + ' Localidade: ' + this.findForm.value.local)
+    alert('Estado : ' + this.findForm.value.stateSelect +
+          ' Cidade : '+ this.findForm.value.citieSelect +
+          ' Caes? :'+ this.findForm.value.enableDog +
+          ' Gatos? :'+ this.findForm.value.enableCat
+        )
   }
 }
