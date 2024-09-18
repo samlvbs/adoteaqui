2e4944fb0d88cb6fe19e0287e720e7a8243bf716
Samuel Vilas Boas Souza <113648081+samlvbs@users.noreply.github.com>
2024-09-18 09:01:19 -0300

    Added automatic cards consuming local db.json to pet pages

--- src/app/pages/caes/caes.component.ts
+++ src/app/pages/caes/caes.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardPetsComponent } from "../../shared/components/card-pets/card-pets.component";
+import { CommonModule } from '@angular/common';
+import * as db from '../../../assets/db.json';
 
 @Component({
   selector: 'app-caes',
   standalone: true,
-  imports: [CardPetsComponent],
+  imports: [CardPetsComponent, CommonModule],
   templateUrl: './caes.component.html',
   styleUrl: './caes.component.scss'
 })
 export class CaesComponent {
-  linkImg: string = '../../../assets/dogteste.jpg';
-  altImg: string = 'Foto de um pet';
-  namePet: string = 'Bobby';
-  infosPet: string = 'Cachorro, 2 anos';
-  localPet: string = 'São Paulo';
+  pets: any[] = [];
 
+  ngOnInit(): void {
+    this.pets = (db as any).pets;
+  }
+
+  getDogs(): any[] {
+    return this.pets.filter(pet => pet.tipo === 'cachorro' && pet.status === 'disponivel');
+  }
 }
--- src/app/pages/gatos/gatos.component.ts
+++ src/app/pages/gatos/gatos.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardPetsComponent } from "../../shared/components/card-pets/card-pets.component";
+import { CommonModule } from '@angular/common';
+import * as db from '../../../assets/db.json';
 
 @Component({
   selector: 'app-gatos',
   standalone: true,
-  imports: [],
+  imports: [CardPetsComponent, CommonModule],
   templateUrl: './gatos.component.html',
   styleUrl: './gatos.component.scss'
 })
 export class GatosComponent {
+  pets: any[] = [];
 
+  ngOnInit(): void {
+    this.pets = (db as any).pets;
+  }
+
+  getCats(): any[] {
+    return this.pets.filter(pet => pet.tipo === 'gato' && pet.status === 'disponivel');
+  }
 }
